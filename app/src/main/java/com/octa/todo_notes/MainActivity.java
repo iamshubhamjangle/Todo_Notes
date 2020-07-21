5f5f0131c9c95ec6294fbecd484683a9d049b8b1
imshubham <jangleshubham@ymail.com>
2020-07-21 19:28:05 +0530

    Added two new fonts. hence two new files in res/fonts applied fonts in resource folder. some minor changes in MainActivity.java

--- app/src/main/java/com/octa/todo_notes/MainActivity.java
+++ app/src/main/java/com/octa/todo_notes/MainActivity.java
@@ -2,13 +2,22 @@ package com.octa.todo_notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView titlepage, subtitlepage, endpage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titlepage = findViewById(R.id.titlepage);
+        subtitlepage = findViewById(R.id.subtitlepage);
+        endpage = findViewById(R.id.endpage);
+
     }
 }
