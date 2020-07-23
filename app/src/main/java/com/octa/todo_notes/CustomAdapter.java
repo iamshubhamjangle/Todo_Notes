02be025b8533af101d71aab5f6db3a8d30ac3641
imshubham <jangleshubham@ymail.com>
2020-07-23 12:01:09 +0530

    added custom Adapter, sql database, and all the design xml

---
+++ app/src/main/java/com/octa/todo_notes/CustomAdapter.java
@@ -0,0 +1,93 @@
+package com.octa.todo_notes;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+    private Context context;
+    private Activity activity;
+    private ArrayList todo_id, todo_title, todo_desc, todo_day;
+
+    CustomAdapter(Activity activity, Context context, ArrayList book_id, ArrayList book_title, ArrayList book_author,
+                  ArrayList book_pages){
+        this.activity = activity;
+        this.context = context;
+        this.todo_id = book_id;
+        this.todo_title = book_title;
+        this.todo_desc = book_author;
+        this.todo_day = book_pages;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_todo, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onBindViewHolder(@NonNull final MyViewHolder holder, final int position) {
+        holder.book_id_txt.setText(String.valueOf(todo_id.get(position)));
+        holder.book_title_txt.setText(String.valueOf(todo_title.get(position)));
+        holder.book_author_txt.setText(String.valueOf(todo_desc.get(position)));
+        holder.book_pages_txt.setText(String.valueOf(todo_day.get(position)));
+        //Recyclerview onClickListener
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(context, UpdateActivity.class);
+//                intent.putExtra("id", String.valueOf(book_id.get(position)));
+//                intent.putExtra("title", String.valueOf(book_title.get(position)));
+//                intent.putExtra("author", String.valueOf(book_author.get(position)));
+//                intent.putExtra("pages", String.valueOf(book_pages.get(position)));
+//                activity.startActivityForResult(intent, 1);
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return todo_id.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView book_id_txt, book_title_txt, book_author_txt, book_pages_txt;
+        LinearLayout mainLayout;
+
+        MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //book_id_txt = itemView.findViewById(R.id.book_id_txt);
+            book_title_txt = itemView.findViewById(R.id.todo_title);
+            book_author_txt = itemView.findViewById(R.id.todo_desc);
+            book_pages_txt = itemView.findViewById(R.id.todo_date);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+
+            //Animate Recyclerview
+            Animation translate_anim = AnimationUtils.loadAnimation(context, R.anim.translate_anim);
+            mainLayout.setAnimation(translate_anim);
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/octa/todo_notes/MainActivity.java
+++ app/src/main/java/com/octa/todo_notes/MainActivity.java
@@ -1,21 +1,73 @@
 package com.octa.todo_notes;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView titlepage, subtitlepage, endpage;
+    RecyclerView recyclerView;
+    //FloatingActionButton add_button;
+    Button add_button;
+    TextView no_data;
+
+    MyDatabaseHelper myDB;
+    ArrayList<String> todo_id, todo_title, todo_desc, todo_day;
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        titlepage = findViewById(R.id.titlepage);
-        subtitlepage = findViewById(R.id.subtitlepage);
-        endpage = findViewById(R.id.endpage);
+        recyclerView = findViewById(R.id.recyclerView);
+        add_button = findViewById(R.id.add_button);
+
+        myDB = new MyDatabaseHelper(MainActivity.this);
+        todo_id = new ArrayList<>();
+        todo_title = new ArrayList<>();
+        todo_desc = new ArrayList<>();
+        todo_day = new ArrayList<>();
+
+        storeDataInArrays();
+
+        customAdapter = new CustomAdapter(MainActivity.this,this, todo_id, todo_title, todo_desc, todo_day);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            recreate();
+        }
+    }
+
+    void storeDataInArrays(){
+        Cursor cursor = myDB.readAllData();
+        if(cursor.getCount() == 0){
+            //empty_imageview.setVisibility(View.VISIBLE);
+            no_data.setVisibility(View.VISIBLE);
+        }else{
+            while (cursor.moveToNext()){
+                todo_id.add(cursor.getString(0));
+                todo_title.add(cursor.getString(1));
+                todo_desc.add(cursor.getString(2));
+                todo_day.add(cursor.getString(3));
+            }
+            //empty_imageview.setVisibility(View.GONE);
+            no_data.setVisibility(View.GONE);
+        }
     }
 }
