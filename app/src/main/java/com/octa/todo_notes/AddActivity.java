1e4836f3849fe2745c44f4d6cf91a7a22f97c88e
imshubham <jangleshubham@ymail.com>
2020-07-24 16:12:30 +0530

    Added date picker imageview button

--- app/src/main/java/com/octa/todo_notes/AddActivity.java
+++ app/src/main/java/com/octa/todo_notes/AddActivity.java
@@ -6,10 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.Calendar;
 
 public class AddActivity extends AppCompatActivity {
 
-    EditText title_input, desc_input, date_input;
+    EditText title_input, desc_input;
+    TextView tv_date_picker;
+    ImageView date_picker;
     Button add_button;
 
     @Override
@@ -19,23 +24,36 @@ public class AddActivity extends AppCompatActivity {
 
         title_input = findViewById(R.id.title_input);
         desc_input = findViewById(R.id.author_input);
-        date_input = findViewById(R.id.pages_input);
+        tv_date_picker = findViewById(R.id.tv_date_picker);
         add_button = findViewById(R.id.add_button);
+        date_picker = findViewById(R.id.imageViewDatePicker);
+        final Calendar myCalendar = Calendar.getInstance();
+
+        date_picker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         add_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                MyDatabaseHelper myDB = new MyDatabaseHelper(AddActivity.this);
-                myDB.addtodo(title_input.getText().toString().trim(),
-                        desc_input.getText().toString().trim(),
-                        date_input.getText().toString().trim());
-                MainActivity.fa.finish();
-
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-                finish();
+                onAddButtonClick();
             }
         });
+    }
+
+    private void onAddButtonClick() {
+        MyDatabaseHelper myDB = new MyDatabaseHelper(AddActivity.this);
+        myDB.addtodo(title_input.getText().toString().trim(),
+                desc_input.getText().toString().trim(),
+                tv_date_picker.getText().toString().trim());
+        MainActivity.fa.finish();
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
     }
 }
